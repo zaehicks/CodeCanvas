33f8697d1b0152ae632183936d3f00e382d58f7e
zaehicks <isaiahhickerson@gmail.com>
2024-01-17 00:22:14 -0500

    create and run backend server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,19 +1,24 @@
-import { Button, Navbar, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
-import { AiOutlineSearch} from 'react-icons/ai'
-import { FaMoon } from 'react-icons/fa'
+import { Button, Navbar, TextInput } from "flowbite-react";
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
 
 const Header = () => {
-    const path = useLocation().pathname;
+  const path = useLocation().pathname;
   return (
     <Navbar className="border-b-2">
-      <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to to-pink-500 rounded-lg text-white">Code</span>
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to to-pink-500 rounded-lg text-white">
+          Code
+        </span>
         Canvas
       </Link>
       <form>
-        <TextInput 
+        <TextInput
           type="text"
           placeholder="Search..."
           rightIcon={AiOutlineSearch}
@@ -28,31 +33,23 @@ const Header = () => {
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" >
-            Sign In
-          </Button>
+          <Button gradientDuoTone="purpleToBlue" outline>Sign In</Button>
         </Link>
         <Navbar.Toggle />
       </div>
-        <Navbar.Collapse>
-          <Navbar.Link active={path === "/"} as={"div"}>
-            <Link to="/">
-              Home
-            </Link>
-          </Navbar.Link>
-          <Navbar.Link active={path === "/about"} as={"div"}>
-            <Link to="/about">
-              About
-            </Link>
-          </Navbar.Link>
-          <Navbar.Link active={path === "/projects"} as={"div"}>
-            <Link to="/projects">
-              Projects
-            </Link>
-          </Navbar.Link>
-        </Navbar.Collapse>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
