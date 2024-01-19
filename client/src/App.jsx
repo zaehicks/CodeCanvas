b6a8ff7111bf90a83b84fe26e58f9c1b468bc761
zaehicks <isaiahhickerson@gmail.com>
2024-01-19 12:57:40 -0500

    created and added footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Dashboard from './pages/Dashboard'
-import Projects from './pages/Projects'
-import Header from './components/Header'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+import FooterComp from "./components/FooterComp";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -19,16 +21,10 @@ const App = () => {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
-
-
-
       </Routes>
-    
+      <FooterComp />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
-
-
-
+export default App;
