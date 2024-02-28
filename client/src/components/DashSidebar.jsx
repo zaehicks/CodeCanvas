ab2b4cdeb01c2fbc790c0d4f097f1f845f71cdf9
zaehicks <isaiahhickerson@gmail.com>
2024-02-27 19:03:10 -0500

    implemented deleteuser functionality to admin

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import { Sidebar } from "flowbite-react";
-import { HiArrowSmRight, HiDocumentText, HiUser } from "react-icons/hi";
+import { HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from "react-icons/hi";
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { signoutSuccess } from "../redux/user/userSlice";
@@ -57,6 +57,19 @@ const DashSidebar = () => {
                 Posts
               </Sidebar.Item>
             </Link>
+            
+          )}
+            {currentUser.isAdmin && (
+            <Link to="/dashboard?tab=users">
+              <Sidebar.Item
+                active={tab === "users"}
+                icon={HiOutlineUserGroup}
+                as="div"
+              >
+                Users
+              </Sidebar.Item>
+            </Link>
+            
           )}
           <Sidebar.Item
             onClick={handleSignout}
