cb5e6977a5d5399476f644ad9e5f927510da2260
zaehicks <isaiahhickerson@gmail.com>
2024-03-05 18:38:53 -0500

    added recent articles to postpage

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,11 +3,10 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/lib/esm/**/*.js',
+    "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require('flowbite/plugin'), require('tailwind-scrollbar')],
-  
+  plugins: [require("flowbite/plugin"), require("tailwind-scrollbar")],
 };
