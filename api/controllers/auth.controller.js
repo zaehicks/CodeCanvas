236afe28ae39c74d459a8a9aede183385d489546
zaehicks <isaiahhickerson@gmail.com>
2024-04-02 12:48:48 -0400

    github auth added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -111,3 +111,60 @@ export const google = async (req, res, next) => {
     next(error);
   }
 };
+
+export const github = async (req, res, next) => {
+  const { email, name, githubPhotoUrl } = req.body; // Extract user data from the request body
+  try {
+    // Check if the user already exists in your database based on their GitHub email
+    const user = await User.findOne({ email });
+
+    if (user) {
+      // If the user exists, generate a JWT token for them
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET
+      );
+
+      // Send the JWT token as a cookie and the user data (excluding password) as JSON
+      const { password, ...rest } = user._doc;
+      res
+        .status(200)
+        .cookie('access_token', token, {
+          httpOnly: true,
+        })
+        .json(rest);
+    } else {
+      // If the user doesn't exist, create a new user in your database
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const newUser = new User({
+        username:
+          name.toLowerCase().split(' ').join('') +
+          Math.random().toString(9).slice(-4),
+        email,
+        password: hashedPassword,
+        profilePicture: githubPhotoUrl,
+      });
+      await newUser.save();
+
+      // Generate JWT token for the new user
+      const token = jwt.sign(
+        { id: newUser._id, isAdmin: newUser.isAdmin },
+        process.env.JWT_SECRET
+      );
+
+      // Send the JWT token as a cookie and the user data (excluding password) as JSON
+      const { password, ...rest } = newUser._doc;
+      res
+        .status(200)
+        .cookie('access_token', token, {
+          httpOnly: true,
+        })
+        .json(rest);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
