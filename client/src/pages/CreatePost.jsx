011024152babbcbfed4de4bd62aece285f29d0ac
zaehicks <isaiahhickerson@gmail.com>
2024-04-02 10:09:20 -0400

    fixed css for html in postpage content section

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -34,7 +34,7 @@ export default function CreatePost() {
     [{ size: ["small", false, "large", "huge"] }], // custom dropdown
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
 
-    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+    [{ color: [] }, { background: [] }], // dropdown with defaults from themez
     [{ align: [] }],
 
     ["clean"], // remove formatting button
